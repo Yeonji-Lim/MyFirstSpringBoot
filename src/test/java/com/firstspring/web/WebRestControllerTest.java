f8f5b50223cbffa100ac8cfbc89864fcf3797e76
YeonjiLim <view7186@naver.com>
2020-07-14 17:38:38 +0900

    ..

--- src/test/java/com/firstspring/web/WebRestControllerTest.java
+++ src/test/java/com/firstspring/web/WebRestControllerTest.java
@@ -1,7 +1,26 @@
 package com.firstspring.web;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = WebRestController.class)
 public class WebRestControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+
+    }
 }
