82054b5fe2b61e7a32df6079f61a4110dd4d3d32
YeonjiLim <view7186@naver.com>
2020-07-21 09:49:53 +0900

    modify readme.md & have to fix error

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # MyFirstSpringBoot
 
-처음 해보는 웹개발
-SpringBoot
-Gradle
-IntelliJ
-JPA
-다 처음 시도해보는 
+처음 해보는 웹개발\
+SpringBoot\
+Gradle\
+IntelliJ\
+JPA\
+다 처음 시도해보는.. \
+
+springboot gradle 웹서비스 만들기 참고 사이트~
+
+https://velog.io/@swchoi0329/Spring-Boot-%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%BD%94%EB%93%9C-%EC%9E%91%EC%84%B1
+https://jojoldu.tistory.com/250
--- src/main/java/com/firstspring/web/WebRestController.java
+++ src/main/java/com/firstspring/web/WebRestController.java
@@ -1,6 +1,8 @@
 package com.firstspring.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //예전에는 @ResponseBody를 모든 메소드에서 적용, 지금은 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줌
@@ -11,5 +13,10 @@ public class WebRestController {
     //예전에는 @RequestMapping(method = RepuestMethod.GET)
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
